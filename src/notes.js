88e446a3b50718893812a3d82070762145012352
Navdeep.K <navdeep152002@gmail.com>
2021-07-19 19:38:40 +0530

    Add Files

---
+++ src/notes.js
@@ -0,0 +1,13 @@
+const Notes = [
+    {
+        key : 1,
+        title: "Yo",
+        content: "HELLO"
+    },
+    {
+        key : 2,
+        title: "Hey",
+        content: "wassup"
+    }
+]
+export default Notes
